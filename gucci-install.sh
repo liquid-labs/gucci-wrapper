e7b2aa3cc36c7a3d7f950f0fca73e9e603b9ec49
Zane Rockenbaugh <zane@liquid-labs.com>
2021-07-22 14:13:29 -0500

    hotfix: tweaked to support linux

--- gucci-install.sh
+++ gucci-install.sh
@@ -3,7 +3,14 @@
 source settings.sh
 
 if [[ $1 == '--force' ]] || ! [[ -f "${GUCCI_BIN}" ]]; then
+  PLATFORM="$(uname -s)"
+  if [[ "${PLATFORM}" == 'Darwin' ]]; then
+    PLATFORM='darwin'
+  else
+    echo "[INFO] Using generic 'linux' platform for '${PLATFORM}'"
+    PLATFORM='linux'
+  fi
   mkdir -p "$(dirname "${GUCCI_BIN}")"
-  curl -L https://github.com/noqcks/gucci/releases/download/${VERSION}/gucci-v${VERSION}-darwin-amd64 -o "${GUCCI_BIN}"
+  curl -L https://github.com/noqcks/gucci/releases/download/${VERSION}/gucci-v${VERSION}-${PLATFORM}-amd64 -o "${GUCCI_BIN}"
   chmod a+x "${GUCCI_BIN}"
 fi
